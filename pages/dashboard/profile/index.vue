66c363ee58e4fc65920b479e692af565182c4bd1
sbsabdulaziz1402 <sbsabdulaziz1402@gmail.com>
2025-09-19 09:59:15 +0500

    correct colors

---
+++ pages/dashboard/profile/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="overflow-y-scroll max-h-[calc(100vh-76px)] flex justify-center">
+        <PhysicalProfile />
+    </div>
+</template>
+
+<script setup>
+import PhysicalProfile from "~/src/widgets/physical-profile/ui.vue";
+
+definePageMeta({
+  layout: "dashboard-layout",
+});
+</script>
+
+<style lang="scss" scoped></style>
